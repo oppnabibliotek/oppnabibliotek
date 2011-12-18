415fe7112920ddb5b1067ad70dad60d249dc9a67
Öppna Bibliotek <kontakt@oppnabibliotek.se>
2011-12-18 22:26:06 +0100

    Explicitly tell Ferret to use utf8, this make swedish characters work in searching.

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Make sure Ferret behaves for swedish characters etc
+Ferret.locale = "en_US.UTF-8"
+
 # Initialize the rails application
 Openlibrary::Application.initialize!
+
